c2cb5ebcf45c2172e9c6d9435c73f7c6c62a6441
ajam06 <gemouhi@gmail.com>
2021-04-02 12:54:31 +0900

    css-loader for adding import in react

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,21 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[fullhash].js',
+    filename: 'bundle.index.js',
   },
   mode: 'development',
   devtool: 'inline-source-map',
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use:  'babel-loader',
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:  'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use:  'css-loader',
+      },
+    ],
   }
 };
